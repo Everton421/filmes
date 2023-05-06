06b6a08f8e7bf42464c780e2264a771fd0187b19
everton421 <evertonjose421@gmail.com>
2023-05-05 21:25:00 -0300

    terceiro commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import api from "../../services/api";
+import './home.css';
 //import axios  from 'axios'
 import { useEffect, useState } from "react";
+import  {Link} from 'react-router-dom'
 
 // https://api.themoviedb.org/3/movie/550?api_key=b5b128dcef2492da021f6f8c079540a0
 // https://api.themoviedb.org/3/movie/now_playing?api_key=b5b128dcef2492da021f6f8c079540a0
@@ -27,15 +29,18 @@ const Home =()=>{
         },[]
     )
     return(
-            <div>
-                {filmes.map( (filme)=>{
-                    return(
-                        <div key={filme.id}>
-                            <p>{filme.title}</p>
-
-                        </div>
-                    );
-                })}
+            <div className="container">
+               <div className="lista-filmes">
+                    {filmes.map( (filme)=> { 
+                        return(
+                            <article>
+                                <strong key={filme.id}> {filme.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                               <Link to={`/filme/${filme.id} `} >Acessar</Link>
+                            </article> 
+                        )
+                        } )}
+               </div>
             </div>
         );
     }
